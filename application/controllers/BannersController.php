031142bf776fa777f8dc182804376504c4135bd0
Marco Antonio <cardona.marco@iexe.edu.mx>
2024-07-03 18:35:26 -0600

    Banner codeigniter

--- application/controllers/BannersController.php
+++ application/controllers/BannersController.php
@@ -7,7 +7,7 @@ class BannersController extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        $this->load->helper('url');
+        $this->load->helper(array('form', 'url'));
         $this->load->library('session');
     }
 
@@ -24,7 +24,21 @@ class BannersController extends CI_Controller
 
     public function insert_banner()
     {
-        phpinfo();
+        $config['upload_path'] = './uploads/';
+        $config['allowed_types'] = 'gif|jpg|png';
+        $config['max_size'] = 2048; // 2MB
+        $config['file_name'] = 'banner_' . time(); // Rename the file to avoid conflicts
+
+        $this->load->library('upload', $config);
+
+        if (!$this->upload->do_upload('banner')) {
+            $error = array('error' => $this->upload->display_errors());
+            echo json_encode($error);
+        } else {
+            $data = array('upload_data' => $this->upload->data());
+            echo json_encode($data);
+        }
+
         echo "<pre>";
         print_r($this->input->post());
         echo "</pre>";
