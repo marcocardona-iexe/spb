372adbebe7f5e68e5030e052bfd2a25c26b8881e
Marco Antonio <cardona.marco@iexe.edu.mx>
2024-08-08 11:46:56 -0600

    Mensajes de seguimiento

--- application/controllers/EstatusAcuerdoController.php
+++ application/controllers/EstatusAcuerdoController.php
@@ -17,15 +17,23 @@ class EstatusAcuerdoController extends CI_Controller
     {
         $session = $this->session->userdata('seguimiento_iexe');
 
+        // Inicializa la variable como 0 (por si el usuario solo tiene el rol de "Administrador")
         $rol_diferente_administrador = 0;
-        foreach ($session['roles'] as $rol) {
-            if ($rol->rol !== 'Administrador') {
-                $rol_diferente_administrador = $rol->idrol;
-                break;
+
+        // Verifica si el usuario tiene otros roles además de "Administrador"
+        if (count($session['roles']) > 1) {
+            foreach ($session['roles'] as $rol) {
+                if ($rol->rol !== 'Administrador') {
+                    $rol_diferente_administrador = $rol->idrol;
+                    break;
+                }
             }
         }
+
+        // Obtener acuerdos según el rol encontrado o 0 si solo es "Administrador"
         $dataAcuerdos = $this->EstatusAcuerdosModel->get_acuerdo_por_rol($rol_diferente_administrador);
 
+        // Retornar los datos como JSON
         echo json_encode($dataAcuerdos);
     }
 }
