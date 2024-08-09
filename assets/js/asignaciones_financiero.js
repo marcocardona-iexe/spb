6178c782da1b5a82d155f82c1e789ddd19ca233f
Marco Antonio <cardona.marco@iexe.edu.mx>
2024-08-09 09:32:41 -0600

    Js para archivo masivo financiero

--- assets/js/asignaciones_financiero.js
+++ assets/js/asignaciones_financiero.js
@@ -1,90 +1,4 @@
 $(document).ready(function () {
-	// $("#subir_archivo").click(function () {
-	// 	// Resetear el botón
-	// 	$("#subir_archivo").html(
-	// 		`<div class="spinner-border spinner-border-sm text-light" role="status"></div> Procesando archivo`
-	// 	);
-
-	// 	var inputFile = $("#excel_financiero")[0].files[0];
-
-	// 	// Validar que se haya seleccionado un archivo
-	// 	if (inputFile) {
-	// 		// Validar el tamaño del archivo (en bytes)
-	// 		// var maxSize = 2 * 1024 * 1024; // 2 MB en bytes
-	// 		// if (inputFile.size > maxSize) {
-	// 		// 	var alertHtml = `<div class="alert alert-warning alert-dismissible fade show" role="alert">
-	// 		//                     <strong>Error:</strong> El archivo seleccionado supera el tamaño máximo permitido de 2 MB.
-	// 		//                     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-	// 		//                 </div>`;
-	// 		// 	$("#form-alert2-container").html(alertHtml);
-	// 		// 	$("#subir_archivo").html(
-	// 		// 		`<i class="fa-solid fa-upload"></i> Subir archivo`
-	// 		// 	);
-	// 		// 	return; // Detener el proceso si el archivo es demasiado grande
-	// 		// }
-
-	// 		var formData = new FormData();
-	// 		formData.append("file", inputFile);
-
-	// 		// Realizar la petición AJAX
-	// 		$.ajax({
-	// 			url: "financiero_masivo",
-	// 			type: "POST",
-	// 			data: formData,
-	// 			processData: false,
-	// 			contentType: false,
-	// 			dataType: "json",
-	// 			success: function (response) {
-	// 				console.log(response);
-	// 				$("#subir_archivo").html(
-	// 					`<i class="fa-solid fa-upload"></i> Subir archivo`
-	// 				);
-	// 				// Manejar la respuesta del servidor
-	// 				var responseObject = response;
-	// 				console.log(responseObject);
-	// 				if (responseObject.status === "error") {
-	// 					var errors = responseObject.errors
-	// 					.map((error) => `<li>${error}</li>`)
-	// 					.join("");
-	// 					var alertHtml = `<div class="alert alert-warning alert-dismissible fade show" role="alert">
-    //                                     <strong>Error:</strong> ${responseObject.message}<ul>${errors}</ul>
-    //                                     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-    //                                 </div>`;
-	// 					$("#form-alert2-container").html(alertHtml);
-	// 				} else {
-	// 					var alertHtml = `<div class="alert alert-success alert-dismissible fade show" role="alert">
-    //                                     <strong>Éxito:</strong> ${responseObject.message}
-    //                                     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-    //                                 </div>`;
-	// 					$("#form-alert2-container").html(alertHtml);
-	// 				}
-	// 			},
-	// 			error: function (xhr, status, error) {
-	// 				$("#subir_archivo").html(
-	// 					`<i class="fa-solid fa-upload"></i> Subir archivo`
-	// 				);
-	// 				// Manejar el error
-	// 				var alertHtml = `<div class="alert alert-warning alert-dismissible fade show" role="alert">
-    //                                 <strong>Error:</strong> Error al procesar el archivo.
-    //                                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-    //                             </div>`;
-	// 				$("#form-alert2-container").html(alertHtml);
-	// 				console.error(error);
-	// 			},
-	// 		});
-	// 	} else {
-	// 		// Mostrar mensaje de error si no se seleccionó ningún archivo
-	// 		var alertHtml = `<div class="alert alert-warning alert-dismissible fade show" role="alert">
-    //                         <strong>Error:</strong> Por favor, seleccione un archivo Excel válido.
-    //                         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-    //                     </div>`;
-	// 		$("#form-alert2-container").html(alertHtml);
-	// 		$("#subir_archivo").html(
-	// 			`<i class="fa-solid fa-upload"></i> Subir archivo`
-	// 		);
-	// 	}
-	// });
-
 	$("#subir_archivo").click(function () {
 		// Resetear el botón
 		$("#subir_archivo").html(
@@ -99,9 +13,9 @@ $(document).ready(function () {
 			// var maxSize = 2 * 1024 * 1024; // 2 MB en bytes
 			// if (inputFile.size > maxSize) {
 			// 	var alertHtml = `<div class="alert alert-warning alert-dismissible fade show" role="alert">
-			//                     <strong>Error:</strong> El archivo seleccionado supera el tamaño máximo permitido de 2 MB.
-			//                     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-			//                 </div>`;
+            //                     <strong>Error:</strong> El archivo seleccionado supera el tamaño máximo permitido de 2 MB.
+            //                     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            //                 </div>`;
 			// 	$("#form-alert2-container").html(alertHtml);
 			// 	$("#subir_archivo").html(
 			// 		`<i class="fa-solid fa-upload"></i> Subir archivo`
@@ -121,11 +35,13 @@ $(document).ready(function () {
 				contentType: false,
 				dataType: "json",
 				success: function (response) {
+					console.log(response);
 					$("#subir_archivo").html(
 						`<i class="fa-solid fa-upload"></i> Subir archivo`
 					);
 					// Manejar la respuesta del servidor
 					var responseObject = response;
+					console.log(responseObject);
 					if (responseObject.status === "error") {
 						var errors = responseObject.errors
 						.map((error) => `<li>${error}</li>`)
@@ -153,7 +69,7 @@ $(document).ready(function () {
                                     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                                 </div>`;
 					$("#form-alert2-container").html(alertHtml);
-					console.log(error);
+					console.error(error);
 				},
 			});
 		} else {
